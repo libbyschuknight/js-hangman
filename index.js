ce0faee050fbc15e6a91d5ba0b0598764e76a9b7
libbyschuknight <libbysk@gmail.com>
2019-06-30 17:35:44 +1200

    Links

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var person = prompt('Please enter your name', 'Harry Potter');
+// https://developer.mozilla.org/en-US/docs/Web/API/Window/prompt
+// www.digitalocean.com/community/tutorials/how-to-write-your-first-javascript-program
+//www.secretgeek.net/console_log
+// https://www.mikedane.com/web-development/javascript/getting-user-input/
+
+http: https: var person = prompt('Please enter your name', 'Harry Potter');
 
 console.log(person);
 
