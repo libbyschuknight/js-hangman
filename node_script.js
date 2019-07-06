1c604658bc4ada97c3d364f44fc02920273ec952
libbyschuknight <libbysk@gmail.com>
2019-07-06 18:58:09 +1200

    Some playing around with user inputs

--- node_script.js
+++ node_script.js
@@ -24,14 +24,65 @@
 // });
 
 // https://flaviocopes.com/node-input-from-cli/
-const readline = require('readline').createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
+// const readline = require('readline').createInterface({
+//   input: process.stdin,
+//   output: process.stdout
+// });
 
-readline.question(`What's your name?`, name => {
-  console.log(`Hi ${name}!`);
-  readline.close();
-});
+// readline.question(`What's your name?`, name => {
+//   console.log(`Hi ${name}!`);
+//   readline.close();
+// });
 
 // https://nodejs.org/api/readline.html
+// https://node.readthedocs.io/en/latest/api/readline/
+// https://github.com/dacogemini/hangman_node_version/blob/master/index.js
+
+// https://www.npmjs.com/package/prompts
+// https://github.com/sanquinluis/week11-hangman/tree/master/node_modules
+// https://github.com/StefanieDing/Week11-Hangman
+
+// const prompts = require('prompts');
+
+// (async () => {
+//   const response = await prompts({
+//     type: 'text',
+//     name: 'guess',
+//     message: 'Make a guess'
+//   });
+
+//   if (response['guess'] === 'a') {
+//     console.log('That was a corrent guess!!');
+//   } else {
+//     console.log('Sorry that was wrong, you lose a life :(');
+//   }
+
+//   // console.log(response); // => { value: 24 }
+// })();
+
+const prompts = require('prompts');
+
+const questions = [
+  {
+    type: 'text',
+    name: 'username',
+    message: 'What is your GitHub username?'
+  },
+  {
+    type: 'number',
+    name: 'age',
+    message: 'How old are you?'
+  },
+  {
+    type: 'text',
+    name: 'about',
+    message: 'Tell something about yourself',
+    initial: 'Why should I?'
+  }
+];
+
+(async () => {
+  const response = await prompts(questions);
+
+  // => response => { username, age, about }
+})();
